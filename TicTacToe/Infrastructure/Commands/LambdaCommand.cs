b5cd7d330b91578659181098d08ace315e731025
VerKarp <vrkrpv@gmail.com>
2022-09-15 16:12:29 +0300

    main command

---
+++ TicTacToe/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TicTacToe.Infrastructure.Commands.Base;
+
+namespace TicTacToe.Infrastructure.Commands
+{
+    internal class LambdaCommand : Command
+    {
+        private readonly Action<object> execute;
+        private readonly Func<object, bool>? canExecute;
+
+        public LambdaCommand(Action<object> execute, Func<object, bool>? canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public override bool CanExecute(object? parameter) => canExecute?.Invoke(parameter) ?? true;
+        public override void Execute(object? parameter) => execute(parameter);
+    }
+}
