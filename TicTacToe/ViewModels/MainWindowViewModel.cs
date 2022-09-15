d6dc39a127641578583599fee6af71204dda35f6
VerKarp <vrkrpv@gmail.com>
2022-09-15 15:48:29 +0300

    main window vm and test

---
+++ TicTacToe/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacToe.ViewModels.Base;
+
+namespace TicTacToe.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+		private string _title = "Tic-Tac-Toe";
+
+		public string Title
+		{
+			get => _title;
+			set => Set(ref _title, value);
+		}
+
+	}
+}
