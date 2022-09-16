64f1456a81bf8e73b11a004949b8c890bf7d7a8e
VerKarp <vrkrpv@gmail.com>
2022-09-16 18:03:19 +0300

    logic for bot working in UI

--- TicTacToe/ViewModels/MainWindowViewModel.cs
+++ TicTacToe/ViewModels/MainWindowViewModel.cs
@@ -18,9 +18,11 @@ namespace TicTacToe.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        private GameMode _gameMode = GameMode.Player;
+
         WindowDialogService _dialog = new();
 
-        private CellState _turn = CellState.NotPressed;
+        private CellState _turn = CellState.Cross;
 
         private bool _isGameOn;
         public bool IsGameOn
@@ -50,12 +52,52 @@ namespace TicTacToe.ViewModels
         public ICommand CloseApplicationCommand { get; }
 		private void OnCloseApplicationCommandExecute(object p) => Application.Current.Shutdown();
 
-
 		public ICommand NewGameCommand { get; }
 		private void OnNewGameCommandExecute(object p) => Board = new(Board.Size);
 
         public ICommand ChangeGameBoardSizeCommand { get; }
-        private void OnChangeGameBoardSizeCommandExecute(object p) => NewGameCommand.Execute(Board.Size = Convert.ToInt32(p));
+        private void OnChangeGameBoardSizeCommandExecute(object p) => 
+            NewGameCommand.Execute(Board.Size = Convert.ToInt32(p));
+
+        public ICommand ChangeGameModeCommand { get; }
+        private void OnChangeGameModeCommandExecute(object p)
+        {
+            switch (Board.Size)
+            {
+                case 3:
+                    Depth = Convert.ToInt32(p) + 4;
+                    break;
+
+                case 4:
+                    Depth = Convert.ToInt32(p) + 1;
+                    break;
+
+                default:
+                    break;
+            }
+            
+            NewGameCommand.Execute(_gameMode = (GameMode)Convert.ToInt32(p));
+        }
+
+        public ICommand GameBoardClickCommand { get; }
+        private void OnGameBoardClickCommandExecute(object p)
+        {
+            if (p is Cell cell)
+            {
+                cell.State = _turn;
+
+                if (_gameMode == GameMode.Player)
+                    _turn = _turn == CellState.Cross ? CellState.Zero : CellState.Cross;
+
+                if (_gameMode == GameMode.EasyBot || _gameMode == GameMode.MediumBot || _gameMode == GameMode.HardBot)
+                    ResultChecker.Bot(Board.Cells, Board.Size, CellState.Zero, Depth);
+
+                CheckWin();
+            }
+        }
+
+        private bool CanGameBoardClickCommandExecuted(object p) =>
+            p is Cell cell && cell.State == CellState.NotPressed;
 
         #endregion
 
@@ -66,21 +108,7 @@ namespace TicTacToe.ViewModels
 			set => Set(ref _gameBoard, value);
 		}
 
-        public ICommand GameBoardClickCommand { get; }
-        private void OnGameBoardClickCommandExecute(object p)
-        {
-            if (p is Cell cell)
-            {
-                cell.State = _turn;
-
-                _turn = _turn == CellState.Cross ? CellState.Zero : CellState.Cross;
-
-                CheckWin();
-            }
-        }
-        
-        private bool CanGameBoardClickCommandExecuted(object p) => 
-            p is Cell cell && cell.State == CellState.NotPressed;
+        public int Depth { get; set; }
 
         private void CheckWin()
         {
@@ -98,6 +126,8 @@ namespace TicTacToe.ViewModels
 
                 if (win == CellState.Zero)
                     _dialog.ShowInformation("TAC!");
+
+                _turn = CellState.Cross;
             }
         }
 
@@ -107,7 +137,7 @@ namespace TicTacToe.ViewModels
 			NewGameCommand = new LambdaCommand(OnNewGameCommandExecute);
             GameBoardClickCommand = new LambdaCommand(OnGameBoardClickCommandExecute, CanGameBoardClickCommandExecuted);
             ChangeGameBoardSizeCommand = new LambdaCommand(OnChangeGameBoardSizeCommandExecute);
-		}
-
+            ChangeGameModeCommand = new LambdaCommand(OnChangeGameModeCommandExecute);
+        }
     }
 }
