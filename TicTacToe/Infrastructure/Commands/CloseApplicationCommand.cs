ed40eeac877bedf3093256c7a3d52b1676aeccb1
VerKarp <vrkrpv@gmail.com>
2022-09-17 13:36:56 +0300

    Close app command in separate file

---
+++ TicTacToe/Infrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using TicTacToe.Infrastructure.Commands.Base;
+
+namespace TicTacToe.Infrastructure.Commands
+{
+    internal class CloseApplicationCommand : Command
+    {
+        public override bool CanExecute(object? parameter) => true;
+
+        public override void Execute(object? parameter) => Application.Current.Shutdown();
+    }
+}
