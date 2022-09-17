51c1254c2446cc178a8777225c4318c4eb687827
VerKarp <vrkrpv@gmail.com>
2022-09-17 13:37:20 +0300

    converter for GameStatus (to bool)

---
+++ TicTacToe/Infrastructure/Converters/GameStatusToBoolConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using TicTacToe.Models.Enums;
+
+namespace TicTacToe.Infrastructure.Converters
+{
+    internal class GameStatusToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not GameStatus gameStatus) return null;
+
+            if (gameStatus == GameStatus.GameOn) return true;
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not bool b) return null;
+
+            if (b == true) return GameStatus.GameOn;
+            return GameStatus.GameOff;
+        }
+    }
+}
