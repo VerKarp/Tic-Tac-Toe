a98b4860202146f260d594b9a5b636fd88404340
VerKarp <vrkrpv@gmail.com>
2022-09-21 12:40:07 +0300

    view model for authorization

---
+++ TicTacToe/ViewModels/AuthorizationViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacToe.ViewModels.Base;
+
+namespace TicTacToe.ViewModels
+{
+    internal class AuthorizationViewModel : ViewModel
+    {
+
+    }
+}
