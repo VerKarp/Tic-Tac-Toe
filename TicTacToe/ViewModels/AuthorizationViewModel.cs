2f89aac62e30df19c2ef1b7b3aa61eba833fcb63
VerKarp <vrkrpv@gmail.com>
2022-11-22 20:15:50 +0300

    authorization view and vm

--- TicTacToe/ViewModels/AuthorizationViewModel.cs
+++ TicTacToe/ViewModels/AuthorizationViewModel.cs
@@ -1,14 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TicTacToe.ViewModels.Base;
+﻿using TicTacToe.ViewModels.Base;
 
 namespace TicTacToe.ViewModels
 {
-    internal class AuthorizationViewModel : ViewModel
+    public class AuthorizationViewModel : ViewModel
     {
+		private string _login;
+		public string Login
+		{
+			get => _login;
+			set => Set(ref _login, value);
+		}
+
+        private string _password;
+        public string Password
+        {
+            get => _password;
+            set => Set(ref _password, value);
+        }
 
     }
 }
---
+++ TicTacToe/Views/AuthorizationView.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Controls;
+
+namespace TicTacToe.Views
+{
+    public partial class AuthorizationView : UserControl
+    {
+        public AuthorizationView() => InitializeComponent();
+    }
+}
