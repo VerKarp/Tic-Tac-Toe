578bba7cb24dfde8e29cd25efbe22fee662f36be
VerKarp <vrkrpv@gmail.com>
2022-11-22 20:38:37 +0300

    added navigation store

---
+++ TicTacToe/Stores/NavigationStore.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TicTacToe.ViewModels.Base;
+
+namespace TicTacToe.Stores
+{
+    public class NavigationStore
+    {
+		public event Action CurrentViewModelChanged;
+
+		private ViewModel _currentViewModel;
+		public ViewModel CurrentViewModel
+        {
+			get => _currentViewModel;
+			set 
+			{ 
+				_currentViewModel = value;
+				CurrentViewModelChanged?.Invoke();
+			}
+		}
+	}
+}
