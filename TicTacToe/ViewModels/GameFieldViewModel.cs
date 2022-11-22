2321e2d21ce27dd0a67a00ea78ddda99f4451349
VerKarp <vrkrpv@gmail.com>
2022-11-22 20:19:45 +0300

    cleaning

--- TicTacToe/ViewModels/GameFieldViewModel.cs
+++ TicTacToe/ViewModels/GameFieldViewModel.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 using TicTacToe.Models.Enums;
 using TicTacToe.Models;
-using TicTacToe.Services;
 using TicTacToe.Infrastructure.Commands;
 using TicTacToe.ViewModels.Base;
 using TicTacToe.Services.Interfaces;
