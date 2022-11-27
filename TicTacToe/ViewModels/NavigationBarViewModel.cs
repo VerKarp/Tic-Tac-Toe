883c4bfe0833fdee564beb380aac3129890203fc
VerKarp <vrkrpv@gmail.com>
2022-11-27 23:37:35 +0300

    navigation bar modified

--- TicTacToe/ViewModels/NavigationBarViewModel.cs
+++ TicTacToe/ViewModels/NavigationBarViewModel.cs
@@ -1,13 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows.Input;
+using TicTacToe.Infrastructure.Commands;
+using TicTacToe.Services.Interfaces;
 using TicTacToe.ViewModels.Base;
 
 namespace TicTacToe.ViewModels
 {
     public class NavigationBarViewModel : ViewModel
     {
+        public ICommand GameNavigateCommand { get; }
+        public ICommand AuthorizationNavigateCommand { get; }
+
+        public NavigationBarViewModel(INavigationService gameFieldNavigationService,
+            INavigationService authorizationNavigationService)
+        {
+            GameNavigateCommand = new NavigateCommand(gameFieldNavigationService);
+            AuthorizationNavigateCommand = new NavigateCommand(authorizationNavigationService);
+        }
     }
 }
