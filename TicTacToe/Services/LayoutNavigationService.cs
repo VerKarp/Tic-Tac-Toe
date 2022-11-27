692a6e348cd246637e4ca66f9dc713a469a0e852
VerKarp <vrkrpv@gmail.com>
2022-11-27 23:36:41 +0300

    added layout navigation service

---
+++ TicTacToe/Services/LayoutNavigationService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using TicTacToe.Services.Interfaces;
+using TicTacToe.Stores;
+using TicTacToe.ViewModels;
+using TicTacToe.ViewModels.Base;
+
+namespace TicTacToe.Services
+{
+    public class LayoutNavigationService<TViewModel> : INavigationService
+        where TViewModel : ViewModel
+    {
+        private readonly NavigationStore _navigationStore;
+        private readonly Func<TViewModel> _createViewModel;
+        private readonly Func<NavigationBarViewModel> _createNavigationBarViewModel;
+
+        public LayoutNavigationService(NavigationStore navigationStore, Func<TViewModel> createViewModel,
+            Func<NavigationBarViewModel> createNavigationBarViewModel)
+        {
+            _navigationStore = navigationStore;
+            _createViewModel = createViewModel;
+            _createNavigationBarViewModel = createNavigationBarViewModel;
+        }
+
+        public void Navigate() =>
+            _navigationStore.CurrentViewModel = 
+            new LayoutViewModel(_createViewModel(), _createNavigationBarViewModel());
+    }
+}
