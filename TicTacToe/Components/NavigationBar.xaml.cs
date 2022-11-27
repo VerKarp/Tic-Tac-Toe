e980ec64cdab3cfadddd37e1798123a6f170049a
VerKarp <vrkrpv@gmail.com>
2022-11-27 20:27:41 +0300

    added navigation bar component (user control)

---
+++ TicTacToe/Components/NavigationBar.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Controls;
+
+namespace TicTacToe.Components
+{
+    public partial class NavigationBar : UserControl
+    {
+        public NavigationBar() => InitializeComponent();
+    }
+}
