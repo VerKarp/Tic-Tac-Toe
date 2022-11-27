ff7703bc4ae1c98c302915d5194bea7ff9a14c38
VerKarp <vrkrpv@gmail.com>
2022-11-28 00:28:35 +0300

    added command

--- TicTacToe/ViewModels/NavigationBarViewModel.cs
+++ TicTacToe/ViewModels/NavigationBarViewModel.cs
@@ -9,12 +9,19 @@ namespace TicTacToe.ViewModels
     {
         public ICommand GameNavigateCommand { get; }
         public ICommand AuthorizationNavigateCommand { get; }
+        public ICommand AboutNavigateCommand { get; }
+        public ICommand SettingsNavigateCommand { get; }
+
 
         public NavigationBarViewModel(INavigationService gameFieldNavigationService,
-            INavigationService authorizationNavigationService)
+            INavigationService authorizationNavigationService,
+            INavigationService aboutNavigationService,
+            INavigationService settingsNavigationService)
         {
             GameNavigateCommand = new NavigateCommand(gameFieldNavigationService);
             AuthorizationNavigateCommand = new NavigateCommand(authorizationNavigationService);
+            AboutNavigateCommand = new NavigateCommand(aboutNavigationService);
+            SettingsNavigateCommand = new NavigateCommand(settingsNavigationService);
         }
     }
 }
