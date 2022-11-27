bce408ef93a3c1caa4ce4b1fb1861ac0e3cfa0f7
VerKarp <vrkrpv@gmail.com>
2022-11-27 20:28:11 +0300

    added navigation bar view model

---
+++ TicTacToe/ViewModels/NavigationBarViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacToe.ViewModels.Base;
+
+namespace TicTacToe.ViewModels
+{
+    public class NavigationBarViewModel : ViewModel
+    {
+    }
+}
