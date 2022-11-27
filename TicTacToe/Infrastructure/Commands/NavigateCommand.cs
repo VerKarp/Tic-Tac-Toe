0b7a5382e4866e1fcb554987b38d824ee8a273a7
VerKarp <vrkrpv@gmail.com>
2022-11-27 23:36:58 +0300

    added navigate command

---
+++ TicTacToe/Infrastructure/Commands/NavigateCommand.cs
@@ -0,0 +1,19 @@
+﻿using TicTacToe.Infrastructure.Commands.Base;
+using TicTacToe.Services.Interfaces;
+
+namespace TicTacToe.Infrastructure.Commands
+{
+    class NavigateCommand : Command
+    {
+        private readonly INavigationService _navigationService;
+        public NavigateCommand(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        public override bool CanExecute(object? parameter) => true;
+
+        public override void Execute(object? parameter) =>
+            _navigationService.Navigate();
+    }
+}
