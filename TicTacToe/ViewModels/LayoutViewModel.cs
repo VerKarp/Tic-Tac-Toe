77be4c217b8d330913142ff5c139a0156fa1c710
VerKarp <vrkrpv@gmail.com>
2022-11-27 20:27:58 +0300

    added layout view model

---
+++ TicTacToe/ViewModels/LayoutViewModel.cs
@@ -0,0 +1,17 @@
+﻿using TicTacToe.ViewModels.Base;
+
+namespace TicTacToe.ViewModels
+{
+    public class LayoutViewModel : ViewModel
+    {
+        public LayoutViewModel(ViewModel contentViewModel, NavigationBarViewModel navigationBarViewModel)
+        {
+            ContentViewModel = contentViewModel;
+            NavigationBarViewModel = navigationBarViewModel;
+        }
+
+        public ViewModel ContentViewModel { get; }
+        public NavigationBarViewModel NavigationBarViewModel { get; }
+
+    }
+}
